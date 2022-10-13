da7310e2c3104ad2aec51938a438db58b10b67f2
ievgend2 <ievgend2@gmail.com>
2022-10-13 17:09:54 -0400

    Created Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <div> Welcome to AACT!</div>
+    <div className="App container">
+      <HomePage/>
     </div>
   );
 }
