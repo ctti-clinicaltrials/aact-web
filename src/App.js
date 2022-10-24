e82bbc23b13436680296b2ef70c098dbdbbcf9d6
ievgend2 <ievgend2@gmail.com>
2022-10-24 07:44:24 -0400

    Recreated App with yarn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <div> Welcome to AACT!</div>
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
