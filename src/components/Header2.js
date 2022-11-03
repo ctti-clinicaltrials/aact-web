fb1aa47bb2e56582095f3eb25cb249b01c1aebf8
Ievgen Dotsenko <66342665+ievgend2@users.noreply.github.com>
2022-11-03 15:48:15 -0400

    Added Learn accordion to the navbar

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,122 +1,124 @@
-import React from 'react'
+import React from "react";
 import image from "../images/full_logo.jpg";
-
 
 export default function Header2() {
   return (
     <nav className="navbar sticky-top bg-white">
-        <div className="container-fluid">
-          <a
-            className="navbar-brand"
-            href="https://www.ctti-clinicaltrials.org/"
-          >
-            <img src={image} alt="Bootstrap" width="200" height="80" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="offcanvas offcanvas-end"
-            tabIndex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                Hi, Ievgen D.
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="connect">
-                    Connect
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="download">
-                    Download
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="learn">
-                    Learn
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="shared_data">
-                    Shared Data
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contactus">
-                    Contact Us
-                  </a>
-                </li>
-
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
+      <div className="container-fluid">
+        <a className="navbar-brand" href="https://www.ctti-clinicaltrials.org/">
+          <img src={image} alt="Bootstrap" width="200" height="80" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              Hi, Ievgen D.
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="connect">
+                  Connect
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="download">
+                  Download
+                </a>
+              </li>
+              {/* <hr class="d-none d-md-block my-2"></hr> */}
+              <li>
+                <div className="nav-item" id="learnAccordion">
+                  <div className="accordion-item">
+                    <div className="accordion-header" id="learnHeading">
+                      <a
+                        className="nav-item collapsed nav-link"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="false"
+                        aria-controls="collapseOne"
+                      >
+                        Learn More About AACT
                       </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <form className="d-flex mt-3" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
+                      <div
+                        id="collapseOne"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="learnHeading"
+                        data-bs-parent="#learnAccordion"
+                      >
+                        <div class="accordion-body mx-4">
+                          <li>
+                            <a className="nav-link" href="#">
+                              Database Schema
+                            </a>
+                          </li>
+                          <li>
+                            <a className="nav-link" href="#">
+                              Data Dcitionary
+                            </a>
+                          </li>
+                          <li>
+                            <a className="nav-link" href="#">
+                              Database Update Schedule
+                            </a>
+                          </li>
+                          <li>
+                            <a className="nav-link" href="#">
+                              Release Notes
+                            </a>
+                          </li>
+                          <li>
+                            <a className="nav-link" href="#">
+                              AACT Before November 15, 2021
+                            </a>
+                          </li>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              {/* <hr class="d-none d-md-block my-2"></hr> */}
+              <li className="nav-item">
+                <a className="nav-link" href="shared_data">
+                  Shared Data
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="contactus">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </nav>
-  )
+      </div>
+    </nav>
+  );
 }
--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,10 +1,14 @@
 import { useRouteError } from "react-router-dom";
+import Footer from "./Footer";
+import Header2 from "./Header2";
 
 export default function NotFoundPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
+    <>
+    <Header2/>
     <div className="text-center">
       <h1>Oops!</h1>
       <p>Sorry, the page you were looking for could not be found..</p>
@@ -12,5 +16,9 @@ export default function NotFoundPage() {
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
+    <div  className="fixed-bottom">
+    <Footer/>
+    </div>
+    </>
   );
 }
